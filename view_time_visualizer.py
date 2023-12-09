b76770a0bfdc8910d76a1dd94ea2b0a2998b76d5
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-09 15:13:57 -0500

    Implemented the function to draw a line plot of daily page views.

--- view_time_visualizer.py
+++ view_time_visualizer.py
@@ -11,3 +11,15 @@ df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
 
 # Clean data
 df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
+
+def draw_line_plot():
+    # Draw line plot
+    fig, ax = plt.subplots(figsize=(15, 5))
+    ax.plot(df.index, df['value'], color='red')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+
+    # Save image and return fig
+    fig.savefig('line_plot.png')
+    return fig
